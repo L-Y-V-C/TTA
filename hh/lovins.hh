d3efc0057d18ec9908a47a8f40d75d4ba6e1538c
L-Y-V-C <yamil.valenzuela.96@gmail.com>
2025-11-12 10:20:17 -0500

    lovinsUpdate

--- hh/lovins.hh
+++ hh/lovins.hh
@@ -8,7 +8,11 @@
 #include <string>
 #include <utility>
 #include <cctype>
+#include <filesystem>
+#include <unordered_set>
+#include <unordered_map>
 
+namespace fs = std::filesystem;
 using namespace std;
 
 class Lovins{
@@ -16,9 +20,13 @@ public:
     Lovins();
     void readAppendixA();
     void readStopwords();
+    void readNews(const string& folderName);
+    string aplyStemming(const string& token);
+    bool evaluateCondition(const string& cond, const string& stem);
+    string recoding(const string& inStem);
 
-    vector<vector<pair<string, string>>> appA;
-    vector<string> stopwords;
+    unordered_map<string, string> appA;
+    unordered_set<string> stopwords;
 };
 
 #endif
