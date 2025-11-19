ac6d3cea6515fb378247c3952877ee78d76e40f0
L-Y-V-C <yamil.valenzuela.96@gmail.com>
2025-11-18 22:41:12 -0500

    R to Rstar

--- main.cpp
+++ main.cpp
@@ -4,19 +4,21 @@
 
 int
 main(){
-    int batchSize = 1500;  // initial batch
+    int batchSize = 20;  // initial batch
     int intervalSec = 1;    // interval between news
-    int topK = 10;          // quantity to show in wordcloud
+    int topK = 30;          // quantity to show in wordcloud
     int lastMNews = 50;    // last M insertions
-    int maxWordsPerFile = 5;
+    int maxWordsPerFile = 3;
 
     TTA tta(batchSize, intervalSec, topK, lastMNews, maxWordsPerFile);
 
     tta.initialize("lovins/stopwords.txt", "lovins/AppendixA.txt");
+    cout<<"===============FINISH INITALIZE================\n";
     tta.loadNewsFiles("AP_BBC_CNN");
+    cout<<"===============FINISH LOAD NEWS FILES================\n";
 
-    string cmd = "start python main.py " + to_string(intervalSec);
-    system(cmd.c_str());
+    //string cmd = "start python main.py " + to_string(intervalSec);
+    //system(cmd.c_str());
 
     tta.processNews();
 
